cb77b719c24994d19ee960a32925f7f065354d23
Olga_Noskova <perhonen_87@mail.ru>
2024-06-28 13:26:04 +0300

    Initial commit

---
+++ test_pytest_yadisk.py
@@ -0,0 +1,61 @@
+import pytest
+import requests
+
+class TestYandexDisk:
+    def setup_method(self):
+        self.headers = {
+            'Authorization': 'OAuth y0_AgAAAAB0PhTFAADLWwAAAAD9GyauAABFMKwdwMdL74S07kEG7swpKVd2Tg'
+        }
+
+    @pytest.mark.parametrize(
+        'param,value,status',
+        (
+                ['pat', 'new_folder', 400],
+                ['path', 'new_folder', 201],
+                ['path', 'new_folder', 409],
+        )
+    )
+    def test_create_folder(self, param, value, status):
+        params = {param: value}
+        response = requests.put('https://cloud-api.yandex.net/v1/disk/resources',
+                                headers=self.headers,
+                                params=params)
+        assert response.status_code == status
+
+    @pytest.mark.parametrize(
+        'param,value,status,headers',
+        (
+                ['path', 'new_folder', 401, {'Authorization': 'OAuth y0'}],
+        )
+    )
+    def test_wrong_token(self, param, value, status, headers):
+        params = {param: value}
+        response = requests.put('https://cloud-api.yandex.net/v1/disk/resources',
+                                headers=headers,
+                                params=params)
+        assert response.status_code == status
+
+
+
+    # def teardown_method(self):
+    #     global params
+    #     response = requests.delete('https://cloud-api.yandex.net/v1/disk/resources',
+    #                             headers=self.headers,
+    #                             params=params)
+
+
+    # @pytest.mark.parametrize(
+    #     'param,value,status',
+    #     (
+    #             ['pat', 'new_folder', 400],
+    #             ['path', 'new folder', 201],
+    #             ['path', 'new folder', 409],
+    #
+    #     )
+    # )
+    # def test_create_folder(self, param, value, status):
+    #     params = {param: value}
+    #     response = requests.put('https://cloud-api.yandex.net/v1/disk/resources',
+    #                             headers=self.headers,
+    #                             params=params)
+    #     assert response.status_code == status
